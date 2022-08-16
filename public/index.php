1dffc93098004bb3b6a91c158c08d53ca5139636
x <karolina_bp@hotmail.com>
2022-08-16 10:48:35 -0300

    Criando um ponto único de entrada

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+//pega o caminho buscado na url
+switch($_SERVER['PATH_INFO']){
+    case '/listar-cursos':
+        require 'listar-cursos.php';
+        break;
+    case '/novo-curso':
+        require 'formulario-novo-curso.php';
+        break;
+    default:
+        echo "Erro 404";
+        break;
+}
