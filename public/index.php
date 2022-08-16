27f2710552917deab9ade5034ed1bba09e38ff35
x <karolina_bp@hotmail.com>
2022-08-16 15:07:00 -0300

    Criando controladores de requisições

--- public/index.php
+++ public/index.php
@@ -1,12 +1,19 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Alura\Cursos\Controller\NovoCurso;
+use Alura\Cursos\Controller\ListarCursos;
+
 //pega o caminho buscado na url
 switch($_SERVER['PATH_INFO']){
     case '/listar-cursos':
-        require 'listar-cursos.php';
+        $controlador = new ListarCursos;
+        $controlador->processaRequisicao();
         break;
     case '/novo-curso':
-        require 'formulario-novo-curso.php';
+        $controlador = new NovoCurso();
+        $controlador->processaRequisicao();
         break;
     default:
         echo "Erro 404";
